4e863a8878a176ad92950f6c59aafda0f3cc9ba3
Alvaro R. S. Dziadzio <dziadzio.alvaro@gmail.com>
2021-06-20 21:02:52 -0300

    📝 Change README.md

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,3 @@
-<script>
-import Tailwind from "./Tailwind.svelte";
-
-</script>
-<Tailwind/>
-
 <main class="max-w-md mx-auto">
 	<h1 class="text-6xl text-center my-8 outline-none">Hello</h1>
 	<p class="w-50 text-center text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe reiciendis veniam aspernatur tempora vero. Placeat neque odit quidem soluta, quibusdam accusamus quod. Culpa nobis provident voluptatibus cum voluptates ut minus?</p>

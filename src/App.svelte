d1db364e1e8afd6b733defa0e562fd0dbad0a758
Alvaro R. S. Dziadzio <dziadzio.alvaro@gmail.com>
2021-06-23 20:25:49 -0300

    🚧 Card character id

--- src/App.svelte
+++ src/App.svelte
@@ -5,7 +5,7 @@ import Card from "./Card.svelte";
 	let p = 0;
 
 	function change() {
-		p += 120;
+		p += 1;
 	}
 
 </script>
@@ -15,21 +15,18 @@ import Card from "./Card.svelte";
 	
 		<h1 class="text-3xl md:text-5xl text-center mb-8 outline-none">Touhou Slot Machine</h1>
 		<p class="text-center text-gray-700 mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe reiciendis veniam aspernatur tempora vero. Placeat neque odit quidem soluta, quibusdam accusamus quod. Culpa nobis provident voluptatibus cum voluptates ut minus?</p>
-	
-		<div class="max-w-md mx-auto grid grid-cols-3 place-content-stretch items-center gap-2">
-		<!-- <div class="max-w-lg mx-auto flex flex-wrap items-center justify-evenly"> -->
-			<Card {p} relation="Has a crush on you"  char="Hall of Dreams Great Mausoleum"/>
-			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
+		<button on:click={change} class="p-3 m-4 rounded-xl bg-indigo-700 text-white font-bold">Change</button>
+
+		<div class="max-w-md mx-auto grid grid-cols-3 place-content-stretch items-center gap-2 sm:gap-6">
+			<Card id={p} relation="Has a crush on you"/>
+			<Card id={p} relation="Married to"/>
+			<Card id={p} relation="Married to"/>
+			<Card id={p} relation="Married to"/>
+			<Card id={p} relation="Married to"/>
+			<Card id={p} relation="Married to"/>
+			<Card id={p} relation="Married to"/>
+			<Card id={p} relation="Married to"/>
+			<Card id={p} relation="Married to"/>
 		</div>
-	
-		<!-- Marisa Kirisame -->
-		<!-- Hall of Dreams Great Mausoleum -->
 	</div>
 </div>
--- src/Card.svelte
+++ src/Card.svelte
@@ -1,14 +1,15 @@
 <script>
+	import data from "./characters.js"
+
 	export let relation;
-	export let char;
-	export let p;
+	export let id;
 
 </script>
 
 <div tabindex="0" style="max-width: 120px;" class="group shadow-md focus:shadow-xl transition-shadow duration-300 rounded-xl overflow-hidden outline-none bg-white">
 	<p class="text-sm p-2 text-center">{relation}</p>
-	<div style="background-position: -{p}px 0px" class="char bg-cover flex flex-col-reverse divide-opacity-5">
-		<p class="group-hover:opacity-100 group-focus:opacity-100 opacity-0 transition-opacity duration-200 text-xs text-center p-1 backdrop-filter backdrop-blur-sm bg-white bg-opacity-60">{char}</p>
+	<div style="background-position: -{id * 120}px 0px" class="char bg-cover flex flex-col-reverse divide-opacity-5">
+		<p class="group-hover:opacity-100 group-focus:opacity-100 opacity-0 transition-opacity duration-200 text-xs text-center p-1 backdrop-filter backdrop-blur-sm bg-white bg-opacity-60">{data[id]}</p>
 	</div>
 </div>
 

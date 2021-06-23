8040514105f6b10d27408183aefb9c801080f4df
Alvaro R. S. Dziadzio <dziadzio.alvaro@gmail.com>
2021-06-23 19:37:10 -0300

    🚧 Better layout

--- src/App.svelte
+++ src/App.svelte
@@ -10,24 +10,26 @@ import Card from "./Card.svelte";
 
 </script>
 
-<main class="max-w-md mx-auto bg-gray-100">
-	<h1 class="text-6xl text-center my-8 outline-none">Touhou Slot Machine</h1>
-	<p class="text-center text-gray-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe reiciendis veniam aspernatur tempora vero. Placeat neque odit quidem soluta, quibusdam accusamus quod. Culpa nobis provident voluptatibus cum voluptates ut minus?</p>
-	<button on:click={change}>Change</button>
-
-	<div class="grid grid-cols-3 place-content-center place-items-center gap-4">
-		<Card {p} relation="Has a crush on you"  char="Hall of Dreams Great Mausoleum"/>
-		<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-		<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-		<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-		<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-		<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-		<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-		<Card {p} relation="Married to"  char="Marisa Kirisame"/>
-		<Card {p} relation="Married to"  char="Marisa Kirisame"/>
+<div class="min-h-screen bg-gray-100">
+	<div class="container mx-auto py-8 px-2 sm:px-8">
+	
+		<h1 class="text-3xl md:text-5xl text-center mb-8 outline-none">Touhou Slot Machine</h1>
+		<p class="text-center text-gray-700 mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe reiciendis veniam aspernatur tempora vero. Placeat neque odit quidem soluta, quibusdam accusamus quod. Culpa nobis provident voluptatibus cum voluptates ut minus?</p>
+	
+		<div class="max-w-md mx-auto grid grid-cols-3 place-content-stretch items-center gap-2">
+		<!-- <div class="max-w-lg mx-auto flex flex-wrap items-center justify-evenly"> -->
+			<Card {p} relation="Has a crush on you"  char="Hall of Dreams Great Mausoleum"/>
+			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
+			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
+			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
+			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
+			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
+			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
+			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
+			<Card {p} relation="Married to"  char="Marisa Kirisame"/>
+		</div>
+	
+		<!-- Marisa Kirisame -->
+		<!-- Hall of Dreams Great Mausoleum -->
 	</div>
-
-	<p>{p}</p>
-	<!-- Marisa Kirisame -->
-	<!-- Hall of Dreams Great Mausoleum -->
-</main>
+</div>
--- src/Card.svelte
+++ src/Card.svelte
@@ -5,16 +5,8 @@
 
 </script>
 
-<div tabindex="0"  style="width: 120px;" class="group shadow-md focus:shadow-xl transition-shadow duration-300 rounded-xl overflow-hidden outline-none bg-white">
-	<p class="text-sm p-2 font-bold text-center">{relation}</p>
-	<!-- <div style="background-position: -{p}px 0px" class="char bg-cover items-end">
-		<p class="text-sm font-bold p-2 text-white bg-gradient-to-b from-black to-transparent">{char}</p>
-	</div> -->
-	<!-- <div style="background-position: -{p}px 0px" class="char bg-cover">
-		<div tabindex="0" class="opacity-0 hover:opacity-100 transition-opacity duration-150 ease-in-out bg-opacity-50 bg-white focus:opacity-100 outline-none p-2 flex justify-center items-center w-full h-full backdrop-filter backdrop-blur-sm">
-			<p class="text-xs text-center">{char}</p>
-		</div>
-	</div> -->
+<div tabindex="0" style="max-width: 120px;" class="group shadow-md focus:shadow-xl transition-shadow duration-300 rounded-xl overflow-hidden outline-none bg-white">
+	<p class="text-sm p-2 text-center">{relation}</p>
 	<div style="background-position: -{p}px 0px" class="char bg-cover flex flex-col-reverse divide-opacity-5">
 		<p class="group-hover:opacity-100 group-focus:opacity-100 opacity-0 transition-opacity duration-200 text-xs text-center p-1 backdrop-filter backdrop-blur-sm bg-white bg-opacity-60">{char}</p>
 	</div>
@@ -22,7 +14,7 @@
 
 <style>
 	.char {
-		width: 120px;
+		/* width: 120px; */
 		height: 120px;
 		background-image: url("/spritesheet.png");
 	}

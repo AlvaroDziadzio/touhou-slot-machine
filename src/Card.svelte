b01177beba18b5decf06df51682475e1ac811e3f
Alvaro R. S. Dziadzio <dziadzio.alvaro@gmail.com>
2021-06-23 20:44:53 -0300

    🚧 Card center character

--- src/Card.svelte
+++ src/Card.svelte
@@ -4,11 +4,13 @@
 	export let relation;
 	export let id;
 
+	let size;
+
 </script>
 
-<div tabindex="0" style="max-width: 120px;" class="group shadow-md focus:shadow-xl transition-shadow duration-300 rounded-xl overflow-hidden outline-none bg-white">
+<div tabindex="0" bind:clientWidth={size} style="max-width: 120px;" class="group shadow-md focus:shadow-xl transition-shadow duration-300 rounded-xl overflow-hidden outline-none bg-white">
 	<p class="text-sm p-2 text-center">{relation}</p>
-	<div style="background-position: -{id * 120}px 0px" class="char bg-cover flex flex-col-reverse divide-opacity-5">
+	<div style="background-position: -{(id * 120 + (120-size) / 2)}px 0px" class="char bg-cover flex flex-col-reverse">
 		<p class="group-hover:opacity-100 group-focus:opacity-100 opacity-0 transition-opacity duration-200 text-xs text-center p-1 backdrop-filter backdrop-blur-sm bg-white bg-opacity-60">{data[id]}</p>
 	</div>
 </div>
